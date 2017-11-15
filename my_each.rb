57d4d2854f71e749c5f66c4f4acd5403d2023b3c
Fiona Lapham <fionadlapham@gmail.com>
2017-11-15 15:08:42 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,13 @@
 def my_each
   if block_given?
     i = 0
+  end
     while i < array.length
       yield(array)
     end
   else
     puts "No block given."
+  end
 end
 
 my_each(array) do |number|
